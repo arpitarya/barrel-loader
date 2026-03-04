10485af8abc20d01de50536555fe08f3531839b1
arpit arya <arpitarya.dev@gmail.com>
2026-03-05 00:47:15 +0530

    fix: reconstruct utils

--- src/ts-utils/reconstruct.ts
+++ src/ts-utils/reconstruct.ts
@@ -12,15 +12,7 @@ function reconstructSource(exports: ExportInfo[]): string {
   }
 
   try {
-    // Convert snake_case to camelCase for native addon and add line field
-    const normalizedExports = exports.map((exp) => ({
-      specifier: exp.specifier,
-      source: exp.source,
-      exportType: exp.export_type,
-      isTypeExport: exp.is_type_export,
-      line: 0, // Line numbers not used in reconstruction
-    })) as unknown as ExportInfo[];
-    return nativeAddon.reconstruct_source_napi('', normalizedExports);
+    return nativeAddon.reconstruct_source_napi('', exports);
   } catch (err) {
     const error = err as Error;
     console.warn('Native addon error, falling back to JavaScript:', error.message);
